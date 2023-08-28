ecbf369c53a9293d28932eb35b8a8adde56a694d
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-08-28 15:12:24 +0530

    modified a little

--- TreeUsinglist.py
+++ TreeUsinglist.py
@@ -6,33 +6,30 @@ while Creating TREE using array ,  we have to remind that:
 '''
 class TreeByList:
     def __init__(self,size) -> None:
-        self.size=size
-        self.root=0
-        self.treelist=[None]
-    
-    def fill_tree(self) -> None:
-        j=1
-        while self.root<=self.size-1:
-            '''
+        '''
             1) Make the current index root
             2) Give choice to user that either 1 or 2 as how many child index it have empty
             3) If user have filled one child , ask again if he/she want to fill another one or not
             else
             4) Give user an option: either 1 or 2 whom he/she will make as new root index and back to step-1
-            '''
-            # choice=int(input("\n\t\t ENTER YOUR CHOICE:"))
-            # match(choice):
-            #     case 1:
-            #         if self.treelist[self.root]==None:
-            #             value=int(input(f"\n\t\tENTER VALUE AT NODE {self.root}: "))
-            #             self.treelist.insert(self.root,value)
-            #             leftchild=2*self.root+1
-            #             rightchild=2*self.root+2
-            #             if leftchild<=self.size-1:
-            #                 self.treelist.insert(leftchild,None)
-            #             if rightchild<=self.size-1:
-            #                 self.treelist.insert(rightchild,None)
-            #         else:
-            #             print("\n\t\tNOT EMPTY")
-            #             continue
-                
+        '''
+        self.size=size
+        self.root=0
+        self.treelist=[None]
+    
+    def fill_tree(self) -> None:
+        value=int(input("\n\t\t Node's Value:"))
+        self.treelist[self.root]=value
+        child_node_list=[self.root*2+1,self.root*2+2]
+        print("\n\t\tPRESS 1 FOR LEFT_CHILD \t\t 2 FOR RIGHT_CHILD\n")
+        child_choice=int(input("\n\t\tWHICH CHILD:"))
+        match(child_choice):
+            case 1:
+                self.root=child_node_list[0]
+                print("LEFT CHILD IS NEW ROOT NODE")
+            case 2:
+                self.root=child_node_list[1]
+                print("RIGHT CHILD IS NEW ROOT NODE")
+            case _:
+                print("\n\t\t SETING LEFT CHILD AS DEFAULT\n")
+
